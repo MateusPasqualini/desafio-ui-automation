9795d55d389056054dc779bb13041c6f4e33d5aa
Mateus Pasqualini <mateus.pasqualini@idt.digital>
2020-04-22 06:27:45 -0300

    :construction: Adicionando package para pageobjects e classe BasePage

---
+++ src/main/java/br/com/southsystem/desafio/pageobjects/BasePage.java
@@ -0,0 +1,33 @@
+package br.com.southsystem.desafio.pageobjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+
+    protected WebDriver driver;
+    private WebDriverWait wait;
+
+
+    BasePage(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    public WebElement waitForVisibilityOf(WebElement element){
+        wait.until(webDriver -> element.isDisplayed());
+        return element;
+    }
+
+    public WebElement waitForClickabilityOf(WebElement element) {
+        wait.until(driverLambda -> element.isDisplayed() && element.isEnabled());
+        return element;
+    }
+
+    public WebElement waitForInvisibilityOf(WebElement element) {
+        wait.until(ExpectedConditions.invisibilityOf(element));
+        return element;
+    }
+}
